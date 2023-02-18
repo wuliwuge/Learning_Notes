2c7dce15abb4ec5df9d1fc4b9b40b558c7dc45b3
wuge <1499065205@qq.com>
2023-02-18 15:01:58 +0800

    copy paste up codes delete self code

--- CMU_15213/BASE_03_04/src/cpu/mmu.h
+++
@@ -1,9 +0,0 @@
-// mmu(memory management unit)：内存管理单元
-#ifndef __MMU__H
-#define __MMU__H
-
-#include <stdint.h>
-
-uint64_t va2pa(uint64_t addr);
-
-#endif /* __MMU__H */
--- CMU_15213/BASE_03_04/src/cpu/register.cpp
+++
@@ -1,3 +0,0 @@
-#include "register.h"
-
-reg_t reg;
